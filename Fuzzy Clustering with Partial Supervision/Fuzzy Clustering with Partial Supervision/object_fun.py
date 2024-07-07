70e5834fae544a165fe6bd96d220f6938cb12fb4
Amin Golzari Oskouei <51666357+Amin-Golzari-Oskouei@users.noreply.github.com>
2024-07-07 18:25:09 +0330

    Add files via upload

---
+++ Fuzzy Clustering with Partial Supervision/Fuzzy Clustering with Partial Supervision/object_fun.py
@@ -0,0 +1,16 @@
+def object_fun(row, col, k, Cluster_elem, center_points, fuzzy_degree, data, f, b, alpha):
+    import math
+    import numpy as np
+
+    distance = np.zeros([k, row, col])
+    dNK = np.zeros([row, k])
+    for j in range(k):
+        distance = (data-np.tile(center_points[j, :], (row, 1))) ** 2
+        dNK[:, j] = np.sqrt(np.sum(distance, 1))
+        
+    j_fun1 = np.sum(np.sum(dNK * np.transpose(Cluster_elem ** fuzzy_degree)))
+    j_fun2 = np.sum(np.sum(dNK * np.transpose((Cluster_elem -   np.transpose(np.transpose(np.tile(b, (k,1))) * f ) ) ** fuzzy_degree)))
+
+    
+    
+    return j_fun1 + (alpha * j_fun2)
